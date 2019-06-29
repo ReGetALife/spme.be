32921f212acee260e44be0c6d6864e651c571d49
Unknown <raxrh1997@163.com>
2019-06-29 20:17:30 +0800

    database interface

--- src/main/java/com/example/demo/ReportController.java
+++ src/main/java/com/example/demo/ReportController.java
@@ -1,11 +1,21 @@
 package com.example.demo;
 
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.CMYKColor;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.*;
 import java.util.List;
 import java.util.Map;
 
@@ -25,13 +35,15 @@ public class ReportController {
     @ResponseStatus(value = HttpStatus.OK)
     public class SqlOKException extends RuntimeException{}
 
+    //获取所有学生
     @CrossOrigin(origins = "*", allowCredentials = "true")
     @RequestMapping(value = "/getStudents", method = RequestMethod.GET)
     public List<Map<String, Object>> getStudents(HttpSession session){
         Object ZOSMF_JSESSIONID = session.getAttribute("ZOSMF_JSESSIONID");
         Object ZOSMF_LtpaToken2 = session.getAttribute("ZOSMF_LtpaToken2");
         Object ZOSMF_Address = session.getAttribute("ZOSMF_Address");
-        if (ZOSMF_JSESSIONID == null || ZOSMF_LtpaToken2 == null || ZOSMF_Address == null) {
+        Object ZOSMF_Account = session.getAttribute("ZOSMF_Account");
+        if (ZOSMF_JSESSIONID == null || ZOSMF_LtpaToken2 == null || ZOSMF_Address == null || ZOSMF_Account == null) {
             //没有token信息，授权失败
             throw new ResourceNotFoundException();
         } else {
@@ -41,18 +53,21 @@ public class ReportController {
         }
     }
 
+    //根据主机号获取学生 参数为要查找的学生主机号
     @CrossOrigin(origins="*", allowCredentials = "true")
-    @RequestMapping(value = "/getStudent/{uid}", method = RequestMethod.GET)
-    public List<Map<String, Object>> getStudentByID(@PathVariable String id, HttpSession session){
+    @RequestMapping(value = "/getStudent", method = RequestMethod.GET)
+    public List<Map<String, Object>> getStudentByID(@RequestBody String uid, HttpSession session){
         Object ZOSMF_JSESSIONID = session.getAttribute("ZOSMF_JSESSIONID");
         Object ZOSMF_LtpaToken2 = session.getAttribute("ZOSMF_LtpaToken2");
         Object ZOSMF_Address = session.getAttribute("ZOSMF_Address");
-        if (ZOSMF_JSESSIONID == null || ZOSMF_LtpaToken2 == null || ZOSMF_Address == null) {
+        Object ZOSMF_Account = session.getAttribute("ZOSMF_Account");
+        System.out.println(ZOSMF_Account);
+        if (ZOSMF_JSESSIONID == null || ZOSMF_LtpaToken2 == null || ZOSMF_Address == null || ZOSMF_Account == null) {
             //没有token信息，授权失败
             throw new UnauthorizedException();
         } else {
-            String sql = "select * from student where uid='" + id + "'";
-            List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
+            String sql = "select * from student where uid=?";
+            List<Map<String, Object>> list = jdbcTemplate.queryForList(sql, uid);
             if(list.size() == 0){
                 throw new ResourceNotFoundException();
             } else {
@@ -61,67 +76,239 @@ public class ReportController {
         }
     }
 
+    //老师提交分数和评论
     @CrossOrigin(origins="*", allowCredentials = "true")
     @RequestMapping(value = "/subScore", method = RequestMethod.POST)
     public List<Map<String, Object>> subScores(@RequestBody Map<String, String> req, HttpSession session){
         Object ZOSMF_JSESSIONID = session.getAttribute("ZOSMF_JSESSIONID");
         Object ZOSMF_LtpaToken2 = session.getAttribute("ZOSMF_LtpaToken2");
         Object ZOSMF_Address = session.getAttribute("ZOSMF_Address");
-        if (ZOSMF_JSESSIONID == null || ZOSMF_LtpaToken2 == null || ZOSMF_Address == null) {
+        Object ZOSMF_Account = session.getAttribute("ZOSMF_Account");
+        Object is_teacher = session.getAttribute("is_teacher");
+        if (ZOSMF_JSESSIONID == null || ZOSMF_LtpaToken2 == null || ZOSMF_Address == null || ZOSMF_Account == null|| is_teacher != "yes") {
             //没有token信息，授权失败
             throw new UnauthorizedException();
         } else {
             String uid = req.get("uid");
+            //String uid = ZOSMF_Account.toString();
             String lab = req.get("lab");
-            String step = req.get("step");
+            String comment = req.get("comment");
             String score = req.get("score");
-            String sql = "update report set score='" + score + "' where uid='" + uid + "' and lab='" + lab + "' and step='" + step + "';";
-            jdbcTemplate.execute(sql);
+            String sql_search = "select * from result where uid=? and lab=?;";
+            List<Map<String, Object>> result_list = jdbcTemplate.queryForList(sql_search, uid, lab);
+            if(result_list.size() == 0){
+                String sql_insert = "insert into result (uid, score, comment, lab) values ( ?, ?, ?, ?);";
+                jdbcTemplate.update(sql_insert, uid, score, comment, lab);
+            }else{
+                String sql_update = "update result set score=?, comment=? where uid=? and lab=?;";
+                jdbcTemplate.update(sql_update, score, comment, uid, lab);
+            }
             throw new SqlOKException();
         }
     }
 
+    //学生生成自己的实验报告
     @CrossOrigin(origins="*", allowCredentials = "true")
-    @RequestMapping(value = "/getreport/{uid}", method = RequestMethod.GET)
-    public List<Map<String, Object>> getReport(@PathVariable String uid, HttpSession session){
+    @RequestMapping(value = "/saveReports", method = RequestMethod.POST)
+    public List<Map<String, Object>> saveReport(@RequestBody Map<String, String> req, HttpSession session){
         Object ZOSMF_JSESSIONID = session.getAttribute("ZOSMF_JSESSIONID");
         Object ZOSMF_LtpaToken2 = session.getAttribute("ZOSMF_LtpaToken2");
         Object ZOSMF_Address = session.getAttribute("ZOSMF_Address");
-        if (ZOSMF_JSESSIONID == null || ZOSMF_LtpaToken2 == null || ZOSMF_Address == null) {
+        Object ZOSMF_Account = session.getAttribute("ZOSMF_Account");
+        if (ZOSMF_JSESSIONID == null || ZOSMF_LtpaToken2 == null || ZOSMF_Address == null || ZOSMF_Account == null) {
             //没有token信息，授权失败
             throw new UnauthorizedException();
         } else {
-            String sql = "select * from report where uid='" + uid + "';";
-            List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
-            if(list.size() == 0){
-                throw new ResourceNotFoundException();
-            } else {
-                return list;
-            }
-        }
-    }
-
-    @CrossOrigin(origins="*", allowCredentials = "true")
-    @RequestMapping(value = "/questions", method = RequestMethod.GET)
-    public List<Map<String, Object>> getQuestions(@RequestBody Map<String, String> req, HttpSession session){
-        Object ZOSMF_JSESSIONID = session.getAttribute("ZOSMF_JSESSIONID");
-        Object ZOSMF_LtpaToken2 = session.getAttribute("ZOSMF_LtpaToken2");
-        Object ZOSMF_Address = session.getAttribute("ZOSMF_Address");
-        if (ZOSMF_JSESSIONID == null || ZOSMF_LtpaToken2 == null || ZOSMF_Address == null) {
-            //没有token信息，授权失败
-            throw new UnauthorizedException();
-        } else {
+            String uid = ZOSMF_Account.toString();
+            //String uid = req.get("uid");
             String lab = req.get("lab");
-            String step = req.get("step");
-            String sql = "select question,answer,uid from report where lab='" + lab + "' and step='" + step + "';";
-            List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
+            String sql = "select * from report natural join question where uid=? and lab=? order by lower_lab, step, question_id";
+            List<Map<String, Object>> list = jdbcTemplate.queryForList(sql, uid, lab);
             if(list.size() == 0){
                 throw new ResourceNotFoundException();
             } else {
-                return list;
+                Document document = new Document();
+                BaseFont bfChinese = null;
+                try{
+                    BaseFont font = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H",BaseFont.NOT_EMBEDDED);
+                    Font cf_lower_lab = new Font(font, 16, Font.BOLD);
+                    Font cf_step = new Font(font, 12, Font.BOLD);
+                    Font cf_question = new Font(font, 8, Font.BOLD);
+                    Font cf_answer = new Font(font, 8, Font.NORMAL);
+                    PdfWriter.getInstance(document, new FileOutputStream("D:\\" + uid + lab + ".pdf"));
+                    document.open();
+                    Paragraph title = new Paragraph(lab, FontFactory.getFont(FontFactory.HELVETICA, 24,
+                            Font.BOLD, new CMYKColor(0, 255, 255, 17)));
+                    Chapter chapter1 = new Chapter(title, 1);
+                    chapter1.setNumberDepth(0);
+                    document.add(chapter1);
+                    Paragraph title_lower;
+                    Section section_lower = chapter1.addSection("");
+                    section_lower.setNumberDepth(0);
+                    Object currentLab = 0, currentStep = 0;
+                    for(Map<String, Object> m : list){
+                        if(currentLab != m.get("lower_lab")){
+                            title_lower = new Paragraph("实验" + m.get("lower_lab"), cf_lower_lab);
+                            if(section_lower !=chapter1.addSection("")){
+                                document.add(section_lower);
+                            }
+                            section_lower = chapter1.addSection(title_lower);
+                            section_lower.setNumberDepth(0);
+                            currentLab = m.get("lower_lab");
+                        }
+                        if(currentStep != m.get("step")){
+                            title_lower = new Paragraph("步骤" + m.get("step"), cf_step);
+                            //title_lower.setFont(font);
+                            section_lower.add(title_lower);
+                            currentStep = m.get("step");
+                        }
+                        title_lower = new Paragraph("题目" + m.get("question_id") + "  " + m.get("question"), cf_question);
+                        section_lower.add(title_lower);
+                        title_lower = new Paragraph("回答:" + m.get("answer"), cf_answer);
+                        section_lower.add(title_lower);
+                    }
+                    document.add(section_lower);
+                    document.close();
+                    //File file = new File("D:\\" + uid + lab + ".pdf");
+                    //file.delete();
+                }catch (DocumentException de){
+                    de.printStackTrace();
+                }catch (FileNotFoundException fnfe){
+                    fnfe.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                throw new SqlOKException();
             }
         }
     }
 
-    
+    //根据主机账号和实验名预览pdf
+    @CrossOrigin(origins="*", allowCredentials = "true")
+    @RequestMapping(value = "/getReports", method = RequestMethod.GET)
+    public String viewPDF(@RequestBody Map<String, String> req, HttpSession session) throws IOException{
+        Object ZOSMF_JSESSIONID = session.getAttribute("ZOSMF_JSESSIONID");
+        Object ZOSMF_LtpaToken2 = session.getAttribute("ZOSMF_LtpaToken2");
+        Object ZOSMF_Address = session.getAttribute("ZOSMF_Address");
+        Object ZOSMF_Account = session.getAttribute("ZOSMF_Account");
+        if (ZOSMF_JSESSIONID == null || ZOSMF_LtpaToken2 == null || ZOSMF_Address == null || ZOSMF_Account == null) {
+            //没有token信息，授权失败
+            throw new UnauthorizedException();
+        }else{
+            String uid = req.get("uid");
+            String lab = req.get("lab");
+            String retString = null;
+            //这里改成服务器上的pdf文件夹路径
+            String dir = "D:\\";
+            String filename = uid + lab + ".pdf";
+            File file = new File(dir + filename);
+            //这里的 D: 改成服务器地址
+            String path = "D:" +  dir.substring(dir.lastIndexOf('\\')) + filename;
+            retString = path.replaceAll("\\\\", "/");
+            return retString;
+        }
+    }
+
+    //提供给老师的接口，批量导入students
+    @CrossOrigin(origins="*", allowCredentials = "true")
+    @RequestMapping(value = "/saveStudents", method = RequestMethod.POST)
+    public String saveStudents(@RequestBody List<Map<String, String>> reqs, HttpSession session){
+        Object ZOSMF_JSESSIONID = session.getAttribute("ZOSMF_JSESSIONID");
+        Object ZOSMF_LtpaToken2 = session.getAttribute("ZOSMF_LtpaToken2");
+        Object ZOSMF_Address = session.getAttribute("ZOSMF_Address");
+        Object ZOSMF_Account = session.getAttribute("ZOSMF_Account");
+        Object is_teacher = session.getAttribute("is_teacher");
+        if (ZOSMF_JSESSIONID == null || ZOSMF_LtpaToken2 == null || ZOSMF_Address == null || ZOSMF_Account == null || is_teacher != "yes") {
+            //没有token信息，授权失败
+            throw new UnauthorizedException();
+        }else{
+        for(Map<String, String> req : reqs){
+            String uid = req.get("uid");
+            String sid = req.get("sid");
+            String sname = req.get("name");
+            String sql = "insert into student (uid, sid, name) values (?, ?, ?);";
+            jdbcTemplate.update(sql, uid, sid, sname);
+        }
+        throw new SqlOKException();
+        }
+    }
+
+    //计算某个实验的提交人数
+    @CrossOrigin(origins="*", allowCredentials = "true")
+    @RequestMapping(value = "/subCount", method = RequestMethod.GET)
+    public int count(@RequestBody String lab, HttpSession session){
+        Object ZOSMF_JSESSIONID = session.getAttribute("ZOSMF_JSESSIONID");
+        Object ZOSMF_LtpaToken2 = session.getAttribute("ZOSMF_LtpaToken2");
+        Object ZOSMF_Address = session.getAttribute("ZOSMF_Address");
+        Object ZOSMF_Account = session.getAttribute("ZOSMF_Account");
+        Object is_teacher = session.getAttribute("is_teacher");
+        if (ZOSMF_JSESSIONID == null || ZOSMF_LtpaToken2 == null || ZOSMF_Address == null || ZOSMF_Account == null || is_teacher != "yes") {
+            //没有token信息，授权失败
+            throw new UnauthorizedException();
+        }else{
+            int count = 0;
+            //改成服务器pdf的文件夹地址
+            String dir = "D:\\";
+            File file_dir = new File(dir);
+            File[] files = file_dir.listFiles();
+            for(int i = 0; i < files.length; i++){
+                if(files[i].isFile()){
+                    String filename = files[i].getName();
+                    if(filename.indexOf(lab) != -1){
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+
+    @CrossOrigin(origins="*", allowCredentials = "true")
+    @RequestMapping(value = "/downloadPDFs", method = RequestMethod.GET)
+    public void downloadPDFs(@RequestBody Map<String, Object> req, HttpSession session, HttpServletResponse response){
+        Object ZOSMF_JSESSIONID = session.getAttribute("ZOSMF_JSESSIONID");
+        Object ZOSMF_LtpaToken2 = session.getAttribute("ZOSMF_LtpaToken2");
+        Object ZOSMF_Address = session.getAttribute("ZOSMF_Address");
+        Object ZOSMF_Account = session.getAttribute("ZOSMF_Account");
+        Object is_teacher = session.getAttribute("is_teacher");
+        if (ZOSMF_JSESSIONID == null || ZOSMF_LtpaToken2 == null || ZOSMF_Address == null || ZOSMF_Account == null || is_teacher != "yes") {
+            //没有token信息，授权失败
+            throw new UnauthorizedException();
+        }else{
+            ServletOutputStream out = null;
+            FileInputStream ips = null;
+            //改成服务器上的pdf文件夹路径
+            String dir = "D:\\";
+            try{
+                String uid = req.get("uid").toString();
+                String lab = req.get("lab").toString();
+                String filename = uid + lab + ".pdf";
+                File file = new File(dir + filename);
+                if(!file.exists()){
+                    return;
+                }else {
+                    ips = new FileInputStream(file);
+                    response.setContentType("multipart/form-data");
+                    response.addHeader("Content-Disposition", "attachment; filename=\"" + uid + lab + ".pdf" + "\"");
+                    out = response.getOutputStream();
+                    //读取文件流
+                    int len = 0;
+                    byte[] buffer = new byte[1024 * 10];
+                    while ((len = ips.read(buffer)) != -1){
+                        out.write(buffer,0,len);
+                    }
+                    out.flush();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }finally {
+                try{
+                    out.close();
+                    ips.close();
+                }catch (IOException ioe){
+                    ioe.printStackTrace();
+                }
+            }
+        }
+    }
 }
