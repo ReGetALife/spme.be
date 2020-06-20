b4bdd563c88dba3af89756edc7fa81eb526e01c0
Yamaa <958457134@qq.com>
2020-06-20 18:02:56 +0800

    修复了一些bug

--- src/main/java/com/spme/dao/ResultDao.java
+++ src/main/java/com/spme/dao/ResultDao.java
@@ -3,6 +3,8 @@ package com.spme.dao;
 import com.spme.Entity.ResultEntity;
 import com.spme.Entity.ResultMultiKeys;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ResultDao extends JpaRepository<ResultEntity, ResultMultiKeys> {
 }
--- src/main/java/com/spme/dao/TeacherDao.java
+++ src/main/java/com/spme/dao/TeacherDao.java
@@ -2,7 +2,9 @@ package com.spme.dao;
 
 import com.spme.Entity.TeacherEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TeacherDao extends JpaRepository<TeacherEntity, String> {
     boolean existsById(String id);
 }
